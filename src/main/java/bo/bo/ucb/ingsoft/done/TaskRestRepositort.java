50af8da474673a17eaf2d9af69dd3d5dde61b42a
Ignacio Paz <ignaciopaz72@gmail.com>
2021-10-04 19:35:06 -0400

    just checking if git is working properly...

---
+++ src/main/java/bo/bo/ucb/ingsoft/done/TaskRestRepositort.java
@@ -0,0 +1,14 @@
+package bo.bo.ucb.ingsoft.done;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "tasks", path = "tasks")
+public interface TaskRestRepositort extends PagingAndSortingRepository<Task, Integer> {
+
+    // List<Task> findAll();
+    List<Task> findByTitle(@Param("title") String title);
+}
